970d66b7c7976248048a2f1ff1918b97582c0097
Athavan Theivakulasingham <44317288+Athiiii@users.noreply.github.com>
2020-10-23 00:44:57 +0200

    Update Dashboard.jsx

--- BZZ-banking.Web/ClientApp/src/components/Dashboard.jsx
+++ BZZ-banking.Web/ClientApp/src/components/Dashboard.jsx
@@ -42,7 +42,7 @@ export class Dashboard extends Component {
         <br></br>
         <p>Account: {user.AccountName}</p>
         <h4>Balance: <span style={{ color: money < 0 ? 'red' : 'green' }}>{(Math.round((money + Number.EPSILON) * 100) / 100).toString().replace(/\B(?=(\d{3})+(?!\d))/g, "'") + " CHF"}</span></h4>
-        <Button color="secondary" className="mt-3 mb-1">
+        <Button color="secondary" className="mt-3 mb-1" onClick={this.getTransactions.bind(this)}>
           <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" className="mr-1" fill="white"><path d="M0 0h24v24H0z" fill="none" /><path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" /></svg>
           Refresh
           </Button>
