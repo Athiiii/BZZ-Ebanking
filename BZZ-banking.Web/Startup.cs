3b3a08173dadece43e690ddf78297f3e49f83e51
Athavan Theivakulasingham <44317288+Athiiii@users.noreply.github.com>
2020-10-23 04:00:00 +0200

    .

--- BZZ-banking.Web/Startup.cs
+++ BZZ-banking.Web/Startup.cs
@@ -39,7 +39,8 @@ namespace BZZ_banking.Web
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseDeveloperExceptionPage();
+              //  app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
